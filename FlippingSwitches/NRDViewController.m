fd94713e2d8f163cbfb147e42411371a4c42e16d
Brendon Justin <brendonjustin@gmail.com>
2014-01-05 19:05:43 -0600

    Finish.

--- FlippingSwitches/NRDViewController.m
+++ FlippingSwitches/NRDViewController.m
@@ -13,6 +13,9 @@
 @property (nonatomic, strong) IBOutletCollection(UISwitch) NSArray *childSwitches;
 @property (nonatomic, weak) IBOutlet UISwitch *masterSwitch;
 
+- (IBAction)masterSwitchValueChanged:(id)sender;
+- (IBAction)childSwitchValueChanged:(id)sender;
+
 @end
 
 @implementation NRDViewController
@@ -27,6 +30,27 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Received actions
+
+- (void)masterSwitchValueChanged:(id)sender
+{
+    UISwitch *theSwitch = sender;
+    
+    BOOL isOn = theSwitch.isOn;
+    [self.childSwitches setValue:@(isOn) forKey:@"on"];
+}
+
+- (void)childSwitchValueChanged:(id)sender
+{
+    NSPredicate *onPred = [NSPredicate predicateWithFormat:@"isOn == YES"];
+    
+    if ([self.childSwitches filteredArrayUsingPredicate:onPred].count == self.childSwitches.count) {
+        [self.masterSwitch setOn:YES animated:YES];
+    } else if (self.masterSwitch.isOn == YES) {
+        [self.masterSwitch setOn:NO animated:YES];
+    }
 }
 
 @end
