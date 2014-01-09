8cdb9639f40c0cca21114565e81ed84789c520d3
Joshua Sullivan <jsulliva@nerdery.com>
2014-01-09 14:03:17 -0600

    Completed challenge.
    Completed the challenge by using a simple counter to keep track of how
    many child switches were on and NSNotifications to broadcast state
    changes on the master switch to the child switches.

---
+++ FlippingSwitches/NRDChildSwitch.h
@@ -0,0 +1,13 @@
+//
+//  NRDChildSwitch.h
+//  FlippingSwitches
+//
+//  Created by Joshua Sullivan on 1/9/14.
+//  Copyright (c) 2014 Ben Dolmar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NRDChildSwitch : UISwitch
+
+@end
--- FlippingSwitches/NRDViewController.m
+++ FlippingSwitches/NRDViewController.m
@@ -7,11 +7,17 @@
 //
 
 #import "NRDViewController.h"
+#import "NRDNotificationConsts.h"
 
 @interface NRDViewController ()
 
 @property (nonatomic, strong) IBOutletCollection(UISwitch) NSArray *childSwitches;
 @property (nonatomic, weak) IBOutlet UISwitch *masterSwitch;
+@property (nonatomic, assign) NSUInteger childCount;
+@property (nonatomic, assign) NSUInteger activeChildSwitches;
+
+- (IBAction)childSwitchChanged:(UISwitch *)sender;
+- (IBAction)masterSwitchChanged:(UISwitch *)sender;
 
 @end
 
@@ -20,13 +26,35 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.activeChildSwitches = 0;
+    self.childCount = [self.childSwitches count];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Event handlers
+
+- (IBAction)childSwitchChanged:(UISwitch *)sender
+{
+    if (sender.on) {
+        self.activeChildSwitches++;
+    } else {
+        self.activeChildSwitches--;
+    }
+    
+    BOOL masterOn = self.activeChildSwitches == self.childCount;
+    [self.masterSwitch setOn:masterOn animated:YES];
+}
+
+- (IBAction)masterSwitchChanged:(UISwitch *)sender
+{
+    [[NSNotificationCenter defaultCenter] postNotificationName:kMasterSwitchState
+                                                        object:self
+                                                      userInfo:@{kMasterStateUserInfoKey:@(sender.on)}];
 }
 
 @end
